227cec622ddfda847814f36fcc2e73287b5ad306
Marcelo-Heitor-de-Almeida-Lira <mhdal.snf23@uea.edu.br>
2026-05-12 10:49:06 -0300

    Add time bomb logic in ship

--- core/entities/__init__.py
+++ core/entities/__init__.py
@@ -2,6 +2,7 @@
 
 from .base import Entity
 from .bullet import Bullet
+from .time_bomb import TimeBomb
 from .asteroid import Asteroid
 from .ship import Ship
 from .ufo import UFO, UFO_BULLET_OWNER
@@ -9,6 +10,7 @@ from .ufo import UFO, UFO_BULLET_OWNER
 __all__ = [
     "Entity",
     "Bullet",
+    "TimeBomb",
     "Asteroid",
     "Ship",
     "UFO",
